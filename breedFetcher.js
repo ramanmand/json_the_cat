0463b685c2d12a9fd478c38cac178017886a90d0
ramanmand <mandr@sheridancollege.ca>
2021-09-27 22:44:40 -0400

    code added

---
+++ breedFetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedChoice,callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedChoice}`;
+  request.get(url,function(err,resp,body) {
+    if (err) {
+      callback(null,err);
+    }  else if (!breedChoice) {
+      callback(err,null);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(err,null);
+      } else {
+        callback(null,data[0].description);
+      }
+       
+    }
+  });
+};
+
+
+module.exports = {fetchBreedDescription};
